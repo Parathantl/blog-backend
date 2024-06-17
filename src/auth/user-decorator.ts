cd1cd33e4aac05220641e84b321b29f3ddb6a009
Parathan THiyagalingam <parathanlive123@gmail.com>
2024-06-18 02:17:49 +0530

    fix: fix eslint format

--- src/auth/user-decorator.ts
+++ src/auth/user-decorator.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 
 export const CurrentUser = createParamDecorator(
@@ -6,3 +7,11 @@ export const CurrentUser = createParamDecorator(
     return request.user;
   },
 );
+=======
+import { ExecutionContext, createParamDecorator } from "@nestjs/common";
+
+export const CurrentUser = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+});
+>>>>>>> 7918796 (feat: initial implementation for blog backend)
